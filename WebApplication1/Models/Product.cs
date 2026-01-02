93f3d90efefcd8491730e0ecd310bfe7e390a0e6
Art'm <kasko.artem45@gmail.com>
2026-01-02 14:51:32 +0300

    add model Product

---
+++ WebApplication1/Models/Product.cs
@@ -0,0 +1,11 @@
+namespace WebApplication1.Models;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Category { get; set; }
+    public string Description { get; set; }
+    public int Price { get; set; }
+    public int Quantity { get; set; }
+    public int Actions { get; set; }
+}
