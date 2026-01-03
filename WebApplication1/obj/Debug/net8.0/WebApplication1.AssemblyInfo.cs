f1ce373cd6e1889bab985b499c9cdd505993a72b
Art'm <kasko.artem45@gmail.com>
2026-01-03 16:31:55 +0300

    added a layout for the table

--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c7ba3715a7bfcdb0d12b5517e447f54c12c7da6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84afa33265097bc491f71f1fa301b7c4da28ae3d")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
